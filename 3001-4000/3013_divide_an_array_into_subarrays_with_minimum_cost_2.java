469675eb279be31855addf90def7760ed26493d6
Divyanshi Dixit <kernelqueen@users.noreply.github.com>
2026-02-01 22:41:43 -0800

    Adding leetcode 3013 solution

--- 3001-4000/3013_divide_an_array_into_subarrays_with_minimum_cost_2.java
+++ 3001-4000/3013_divide_an_array_into_subarrays_with_minimum_cost_2.java
@@ -1,54 +1,63 @@
 class Solution {
     public long minimumCost(int[] nums, int k, int dist) {
-        TreeSet<Integer> currset =
-    new TreeSet<>((a,b) -> nums[a] != nums[b] ? nums[a] - nums[b] : a - b);
-
-TreeSet<Integer> futset =
-    new TreeSet<>((a,b) -> nums[a] != nums[b] ? nums[a] - nums[b] : a - b);
-
+        // O(n)
+        TreeSet<Integer> currset = new TreeSet<>((a,b)-> nums[a]!=nums[b] ? nums[a]-nums[b] : a-b);
+        TreeSet<Integer> futset = new TreeSet<>((a,b)-> nums[a]!=nums[b] ? nums[a]-nums[b] : a-b);
         long cost = Long.MAX_VALUE;
+
         int n = nums.length;
-        long currsum = 0l;
+        long currsum = 0l; // sum of cost of all subarays in a wind
+
+        // populate currset and futset
+        // currser = top k-1 min elements
+        // futset = rem elements
 
         for(int i=1; i<=dist+1; i++) {
             currset.add(i);
             currsum += nums[i];
         }
+
         while(currset.size() > k-1) {
-            // removing max values and adding to futset
             int ind = currset.pollLast();
-            currsum -=nums[ind];
+            currsum -= nums[ind];
             futset.add(ind);
         }
-        cost = Math.min(currsum, cost);
-        // need k-1 lowest numbers with window size = dist+1
-        for(int i=1; i+dist+1 < n ; i++) {
+
+        cost = currsum;
+        // O(n*logn)
+        for(int i=1; i < n-1-dist; i++) {
             int rightInd = i+dist+1;
-            if(currset.contains(i)) {
-                currsum -= nums[i];
-                currset.remove(i);
+            int leftInd = i;
+            if(currset.contains(leftInd)) {
+                currsum -= nums[leftInd];
+                currset.remove(leftInd);
                 currsum += nums[rightInd];
                 currset.add(rightInd);
             }
             else {
-                futset.remove(i);
+                futset.remove(leftInd);
                 futset.add(rightInd);
             }
-            
+
+            // balancing
             if(futset.size() > 0) {
                 int minF = futset.first();
                 int maxC = currset.last();
                 if(nums[minF] < nums[maxC]) {
                     currsum -= nums[maxC];
                     currset.remove(maxC);
-                    futset.add(maxC);
-                    currsum += nums[minF];
                     currset.add(minF);
+                    currsum += nums[minF];
                     futset.remove(minF);
+                    futset.add(maxC);
                 }
             }
-            cost = Math.min(cost, currsum); 
+            
+            
+            cost = Math.min(cost, currsum);
         }
+
         return cost + nums[0];
+    
     }
 }
