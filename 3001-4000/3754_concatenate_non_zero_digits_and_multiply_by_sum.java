042d8584b20a8a79d76e334438c91535fe582d14
kernelqueen <kernelqueen@users.noreply.github.com>
2026-07-06 17:58:46 -0700

    Adding leetcode 3754 solution

---
+++ 3001-4000/3754_concatenate_non_zero_digits_and_multiply_by_sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long sumAndMultiply(int n) {
+        String s = Integer.toString(n);
+
+        // O(logn)
+
+        long ans = 0, sum = 0;
+
+        for(int i=0; i<s.length(); i++) {
+            int digit = s.charAt(i)-'0';
+            if(digit!=0) {
+                sum += digit;
+                ans = ans*10 + digit;
+            }
+        }
+
+        return sum*ans;
+    }
+}
